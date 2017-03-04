d277344db6397166bdde7a1a1b02cf25545763de
saeed <saeed@yahoo.com>
2017-03-04 21:41:31 +0100

    final

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -7,7 +7,7 @@ class Footer1 extends Component {
   render(){
     return (
         <div className="footer">
-            <Well><p>Design And Developed by <a href="http://www.geomap.ml/"> Saeed Sharifi</a>. Copyright &copy; 2017. All Rights Reserved.</p></Well>
+            <Well><p>Design And Developed by <a href="http://www.geomap.ml/"> Saeed Sharifi</a>. Copyright &copy; 2017. All Rights Reserved.<a href="https://github.com/moh0040/LiveBus.git"> Project on GitHub</a></p></Well>
         </div>
 
     );
